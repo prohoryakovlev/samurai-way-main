583fdaca69247dd6375b7172abd05afd2ededbe3
Prohor <prohor-yakovlev@mail.ru>
2022-11-07 22:01:28 +0300

    Add header and technologies

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,38 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+            <div className="App">
+
+            </div>
+        </div>
+    );
+}
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <div>
+            <a>Home</a>
+            <a>News Feed</a>
+            <a>Messages</a>
+        </div>
+    )
 }
 
 export default App;

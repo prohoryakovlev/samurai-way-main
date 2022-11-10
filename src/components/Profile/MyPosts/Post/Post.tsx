a5e9848959626637e99801a2e6985ad1d91d0796
Prohor <prohor-yakovlev@mail.ru>
2022-11-10 21:07:12 +0300

    Add myPost

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -4,10 +4,13 @@ import s from "./Post.module.css";
 
 const Post = () => {
     return (
-                    <div className={s.item}>
-                        <img src="https://cs6.pikabu.ru/avatars/298/v298790.jpg"/>
-                        post 1
-                    </div>
+        <div className={s.item}>
+            <img src="https://cs6.pikabu.ru/avatars/298/v298790.jpg"/>
+            post 1
+            <div>
+                <span>Like</span>
+            </div>
+        </div>
     )
 }
 export default Post

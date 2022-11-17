c48fce3d9b3570a7ad23ab2e9a9779c2e7fd78a7
Prohor <prohor-yakovlev@mail.ru>
2022-11-17 17:31:53 +0300

    Add activeClass

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,36 @@ import s from './Dialogs.module.css'
 const Dialogs = () => {
     return (
         <div className={s.content}>
-            Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' ' + s.active}>
+                        Dimych
+                    </div>
+                    <div className={s.dialog}>
+                        Andrey
+                    </div>
+                    <div className={s.dialog}>
+                        Sveta
+                    </div>
+                    <div className={s.dialog}>
+                        Sasha
+                    </div>
+                    <div className={s.dialog}>
+                        Victor
+                    </div>
+                    <div className={s.dialog}>
+                        Valera
+                    </div>
+
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How us your it-camasutra</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
+
     )
 }
 

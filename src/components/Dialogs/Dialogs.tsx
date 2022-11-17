cf7ba728fc52bbace63595053b8919c9b9a9c0cd
Prohor <prohor-yakovlev@mail.ru>
2022-11-17 20:42:08 +0300

    Add Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,36 +2,50 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+type DialogTtem = {
+    name: string
+    id: number
+}
+type Message = {
+    name: string
+    message: string
+}
+
+
+const DialogItem = (props: DialogTtem) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}> {props.name}</NavLink>
+        </div>
+    )
+}
+
+const Message = (props: any) => {
+    return (
+        <div className={s.dialogs}>{props.message}</div>
+    )
+}
+
 const Dialogs = () => {
     return (
-        <div className={s.content}>
-            <div className={s.dialogs}>
-                <div className={s.dialogsItems}>
-                    <div className={s.dialog + ' ' + s.active}>
-                        <NavLink to={"/dialogs/1"}> Dimych</NavLink>
-                    </div>
-                    <div className={s.dialog}>
-                        <NavLink to={"/dialogs/2"}>Andrey</NavLink>
-                    </div>
-                    <div className={s.dialog}>
-                        <NavLink to={"/dialogs/3"}>Sveta</NavLink>
-                    </div>
-                    <div className={s.dialog}>
-                        <NavLink to={"/dialogs/4"}>Sasha</NavLink>
-                    </div>
-                    <div className={s.dialog}>
-                        <NavLink to={"/dialogs/5"}>Victor</NavLink>
-                    </div>
-                    <div className={s.dialog}>
-                        <NavLink to={"/dialogs/6"}>Valera</NavLink>
-                    </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogItem name={"PROHOR"} id={1}/>
+                <DialogItem name={"Andrey"} id={2}/>
+                <DialogItem name={"Sveta"} id={3}/>
+                <DialogItem name={"Sasha"} id={4}/>
+                <DialogItem name={"Victor"} id={5}/>
+                <DialogItem name={"Valera"} id={6}/>
 
-                </div>
+
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>How us your it-camasutra</div>
-                <div className={s.message}>Yo</div>
+                <Message message={"Hi"}/>
+                <Message message={"How us your it-camasutra"}/>
+                <Message message={"Yo"}/>
+                <Message message={"Yo"}/>
+                <Message message={"Yo"}/>
             </div>
         </div>
 

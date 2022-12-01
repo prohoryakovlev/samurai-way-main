409708125b60bd64bc51112283782636d0165257
Prohor <prohor-yakovlev@mail.ru>
2022-12-01 17:58:41 +0300

    Add PostData

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -28,21 +28,36 @@ const Message = (props: any) => {
 }
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: "PROHOR"},
+        {id: 2, name: "Andrey"},
+        {id: 3, name: "Sveta"},
+        {id: 4, name: "Sasha"},
+        {id: 5, name: "Victor"},
+        {id: 6, name: "Valera"},
+    ]
+
+    let messagesData = [
+        {id: 1, message: "Hi"},
+        {id: 2, message: "How us your it-kamasutra"},
+        {id: 3, message: "Yo"},
+        {id: 4, message: "Yo"},
+        {id: 5, message: "Yo"},
+        {id: 6, message: "Yo"},
+    ]
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={"PROHOR"} id={1}/>
-                <DialogItem name={"Andrey"} id={2}/>
-                <DialogItem name={"Sveta"} id={3}/>
-                <DialogItem name={"Sasha"} id={4}/>
-                <DialogItem name={"Victor"} id={5}/>
-                <DialogItem name={"Valera"} id={6}/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+
 
 
             </div>
             <div className={s.messages}>
-                <Message message={"Hi"}/>
-                <Message message={"How us your it-camasutra"}/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
                 <Message message={"Yo"}/>
                 <Message message={"Yo"}/>
                 <Message message={"Yo"}/>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import s from "./MyPosts.module.css";
-import Post from "./Post/Post";
+import Post from "./Post/Post"
 import {message} from "antd";
 import {log} from "util";
 
@@ -14,6 +14,13 @@ import {log} from "util";
 
 const MyPosts = (props: any) => {
     console.log(props.hey)
+
+    let postData = [
+        {id: 1, message: "Hi, how are you?", likesCount: 12},
+        {id: 2, message: "Its my first post", likesCount: 11},
+
+    ]
+
     return (
         <div className={s.postsBlock}>
             <h3>My Post</h3>
@@ -25,8 +32,8 @@ const MyPosts = (props: any) => {
             </div>
 
             <div className={s.posts}>
-                <Post message={"Hi, how are you?"} likesCount={0}/>
-                <Post message={"Its my first post"} likesCount={23}/>
+                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
 
 
             </div>
